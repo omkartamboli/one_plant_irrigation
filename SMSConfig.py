52b82e9f51a26d420b397b7c1555a41d6934ce38
Omkar Tamboli <omkar.tamboli@cognizant.com>
2016-08-02 14:21:20 +0100

    Add SMS notification functionality

---
+++ SMSConfig.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# We are using twilio SMS gateway to send SMS, if you dont want this feature remove the SMS code.
+# If you want to use twilio, register free at https://www.twilio.com/
+# We don't support or advise using this service, it's user preference
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+ACCOUNT_SID = '<Your Account SID provided by twilio>'
+AUTH_TOKEN = '<Your auth token provided by twilio>'
+
+
+TO_NUMBER = '<Your verified number with twilio>'
+FROM_NUMBER = '<Number Provided by twilio to you>'
+
+
+
+# The next three variables use triple quotes, these allow us to preserve the line breaks in the string.
+
+
+# This is the message that will be sent when moisture IS detected again
+
+sms_message_alive = """Panic over! Plant has water again :)"""
+
+
+# This is the message that will be sent when NO moisture is detected, and opening the tap to water plant
+
+sms_message_opening_tap = """Warning, no moisture detected!
+Opening tap to water plant!!!
+"""
+
+
+# This is the message that will be sent when NO moisture is detected evenafter watering plant
+
+sms_message_dead = """Warning, no moisture detected, after watering plant.
+Please check water levels.
+
+Plant death imminent!!! :'(
+"""
