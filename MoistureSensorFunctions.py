660c0b6115d998697eed44ef2fab750355eca8bb
Omkar Tamboli <omkar.tamboli@cognizant.com>
2016-08-02 13:38:24 +0100

    Add main file to execute entire logic

--- MoistureSensorFunctions.py
+++ MoistureSensorFunctions.py
@@ -1,6 +1,6 @@
 # Import required libraries
-from GPIOConfig import *
 from EmailFunctions import *
+from StepperFunctions import *
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -8,11 +8,27 @@ from EmailFunctions import *
 # ---------------------------------------------------------------------------------------------------------------------
 
 def callback(MoisturePin):
+
     if GPIO.input(MoisturePin):
         print "LED off"
-        send_email(message_dead)
+
+        # Send email about watering plant
+        send_email(message_opening_tap)
+
+        # Open tap for 1 second
+        openAndCloseTap(1)
+
+        # Wait for 10 seconds for water to be absorbed by soil
+        time.sleep(10)
+
+        # check the soil moisture level after watering the plant, send email if moisture level is not restored
+        if GPIO.input(MoisturePin):
+            send_email(message_dead)
+
+
     else:
         print "LED on"
+        # Send email if moisture level is ok and no watering is required
         send_email(message_alive)
 
 
---
+++ OnePlantIrrigation.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# This is main file which will use all other utilities to detect the moisture and turnon the tap if moisture is less
+# This will also send email notifications about current status of the plant
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+from MoistureSensorFunctions import *
+
+# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(MoisturePin, GPIO.BOTH, bouncetime=300)
+
+# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin,
+# run this function
+GPIO.add_event_callback(MoisturePin, callback)
+
+# This is an infinite loop to keep our script running
+# TODO: Convert to cronjob
+
+while True:
+    # This line simply tells our script to wait 2 minutes, this is so the script doesnt hog all of the CPU
+    time.sleep(120)
