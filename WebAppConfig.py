705cd019a129a6f0f4166d5ce5d875e11de2e320
Omkar Tamboli <omkar.tamboli@cognizant.com>
2016-08-17 19:25:18 +0100

    adding api key

--- WebAppConfig.py
+++ WebAppConfig.py
@@ -2,3 +2,4 @@ ssl_keyfile_location = '<your ssl key file location>'
 ssl_certfile_location = '<your ssl certificate file location>'
 server_host = '0.0.0.0'
 server_port = 5000
+turnOnTapApiKey = '<your preffered API key for app>'
